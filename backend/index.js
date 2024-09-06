90e452a5d79c3c0a3938c6b1bed1b82a7e4155ca
Ashish-Sehray <sehrayashish54@gmail.com>
2024-09-06 18:21:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,7 @@ app.get("/", (req, res) => {
   res.send("server is working");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT|| 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
